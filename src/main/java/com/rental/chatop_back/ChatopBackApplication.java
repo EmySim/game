4391712645ae8006b0f7a0060a75ac3225c33070
EmySim <emiliesimson@gmail.com>
2025-02-03 15:22:49 +0100

    Initial commit

---
+++ src/main/java/com/rental/chatop_back/ChatopBackApplication.java
@@ -0,0 +1,13 @@
+package com.rental.chatop_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatopBackApplication.class, args);
+	}
+
+}
