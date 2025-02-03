c767a28e44c0674dbf06d9139bb07dd5c4e0e651
EmySim <emiliesimson@gmail.com>
2025-02-04 00:49:16 +0100

    ajout jwtservice

---
+++ src/main/java/com/rental/chatop_back/configuration/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.rental.chatop_back.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Utilisation de BCrypt pour encoder les mots de passe
+    }
+}
--- src/main/java/com/rental/chatop_back/service/UserService.java
+++ src/main/java/com/rental/chatop_back/service/UserService.java
@@ -11,20 +11,33 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    // Injection des dépendances via le constructeur
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     // Méthode d'enregistrement de l'utilisateur
     public User register(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword())); // Encodage du mot de passe
-        return userRepository.save(user); // Sauvegarde de l'utilisateur dans la base
+        // Vérification si l'email est déjà utilisé
+        Optional<User> existingUser = findByEmail(user.getEmail());
+        if (existingUser.isPresent()) {
+            throw new IllegalArgumentException("Cet email est déjà utilisé.");
+        }
+
+        // Encodage du mot de passe avant de sauvegarder l'utilisateur
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        // Sauvegarde de l'utilisateur dans la base de données
+        return userRepository.save(user);
     }
 
     // Méthode pour rechercher un utilisateur par son email
     public Optional<User> findByEmail(String email) {
-        return userRepository.findByEmail(email); // Recherche par email
+        return userRepository.findByEmail(email);
     }
 }
