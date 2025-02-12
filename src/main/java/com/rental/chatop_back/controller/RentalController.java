062906ec6ce38daad3b58cd12d6b64d8b96fa346
EmySim <emiliesimson@gmail.com>
2025-02-12 16:36:42 +0100

    séparation filtre routes publiques/avec jwt, modifications CORS et ajout rentalcontroller

--- src/main/java/com/rental/chatop_back/controller/RentalController.java
+++ src/main/java/com/rental/chatop_back/controller/RentalController.java
@@ -1,5 +1,10 @@
 package com.rental.chatop_back.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/rentals")
 public class RentalController {
