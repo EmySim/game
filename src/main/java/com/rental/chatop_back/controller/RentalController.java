924dcd03b8f0591a7141a4ccf534ca5c591509c1
EmySim <emiliesimson@gmail.com>
2025-02-13 14:35:35 +0100

    ajout commandLineCOntroller

--- src/main/java/com/rental/chatop_back/controller/RentalController.java
+++ src/main/java/com/rental/chatop_back/controller/RentalController.java
@@ -1,18 +1,28 @@
 package com.rental.chatop_back.controller;
 
+import com.rental.chatop_back.model.Rental;
+import com.rental.chatop_back.service.RentalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/rentals")
 public class RentalController {
 
+    private final RentalService rentalService;
+
+    @Autowired
+    public RentalController(RentalService rentalService) {
+        this.rentalService = rentalService;
+    }
+
     @GetMapping
-    public ResponseEntity<String> getRentals() {
-        return ResponseEntity.ok("{\n" +
-                "  \"message\": \"Rental created !\"\n" +
-                "}");
+    public ResponseEntity<List<Rental>> getAllRentals() {
+        List<Rental> rentals = rentalService.getAllRentals();
+        return ResponseEntity.ok(rentals);
     }
 }
